0798408132f1bfe3734c7038e57b3bbb61a5ddc2
Jonas Gabriel <jonas@wedsys.com.br>
2022-12-11 09:15:46 -0300

    criacao de validador de input para deletar caso esteja vazio e organizacao de taskList checked

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 import { TaskList } from '../../model/task-list';
 
@@ -7,13 +7,14 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements DoCheck {
 
   public taskList: Array<TaskList> = [];
 
   constructor() { }
 
-  OnInit(): void {
+  ngDoCheck(): void {
+    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked))
   }
 
   public setEmitTaskList(event: string) {
@@ -32,6 +33,16 @@ export class TodoListComponent {
 
     if (confirm) {
       this.taskList = []
+    }
+  }
+
+  public validateInput (event: string, index: number) {
+    if (!event.length) {
+      const confirm = window.confirm('A task está vazia, deseja deletar?')
+
+      if (confirm) {
+        this.deleteItemTaskList(index)
+      }
     }
   }
 
